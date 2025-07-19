0763d7c448a0c6109dba72bfb5fb2d8d9590dc06
jubilcabrestante <jubilcabrestante.dev@gmail.com>
2025-07-19 18:25:48 +0800

    update social media links

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -2,30 +2,42 @@ import Link from "next/link";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import styles from "./social-media.module.css";
 
+type SocialLink = {
+  href: string;
+  icon: React.ReactNode;
+  ariaLabel: string;
+};
+
 const SocialMedia = () => {
+  const socialLinks: SocialLink[] = [
+    {
+      href: "https://www.facebook.com/jubil18",
+      icon: <FaFacebookF className="w-4 h-4" />,
+      ariaLabel: "Facebook",
+    },
+    {
+      href: "https://www.linkedin.com/in/jubil-cabrestante-4a06b4363/",
+      icon: <FaLinkedinIn className="w-4 h-4" />,
+      ariaLabel: "LinkedIn",
+    },
+  ];
+
   return (
     <ul>
       <li>
         <div className="flex space-x-4">
-          <Link
-            href="https://www.facebook.com/jubil18"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.iconLink}
-            aria-label="Facebook"
-          >
-            <FaFacebookF className="w-4 h-4" />
-          </Link>
-
-          <Link
-            href="https://www.linkedin.com/in/jubil-cabrestante-4a06b4363/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.iconLink}
-            aria-label="LinkedIn"
-          >
-            <FaLinkedinIn className="w-4 h-4" />
-          </Link>
+          {socialLinks.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.iconLink}
+              aria-label={link.ariaLabel}
+            >
+              {link.icon}
+            </Link>
+          ))}
         </div>
       </li>
     </ul>
